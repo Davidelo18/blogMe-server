81564d729a0f05c0dfc4a3442d63b01ba0abd91d
Dawid <daw.kielbik@gmail.com>
2021-07-20 22:57:09 +0200

    Poprawienie bazy danych

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    MONGODB: `mongodb+srv://dkielbik:FgaZcrHGpJ6aSsA@cluster0.jhurn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
+    MONGODB: `mongodb+srv://dkielbik:FgaZcrHGpJ6aSsA@cluster0.jhurn.mongodb.net/blogme?retryWrites=true&w=majority`
 }
