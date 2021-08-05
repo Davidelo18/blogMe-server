6d4913618510e00f5b716a5b735211d1e7dd171d
Dawid <daw.kielbik@gmail.com>
2021-08-06 00:24:45 +0200

    Secret key

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
 module.exports = {
-    MONGODB: `mongodb+srv://dkielbik:FgaZcrHGpJ6aSsA@cluster0.jhurn.mongodb.net/blogme?retryWrites=true&w=majority`
+    MONGODB: `mongodb+srv://dkielbik:FgaZcrHGpJ6aSsA@cluster0.jhurn.mongodb.net/blogme?retryWrites=true&w=majority`,
+    SECRET_KEY: 'very secret key'
 }
